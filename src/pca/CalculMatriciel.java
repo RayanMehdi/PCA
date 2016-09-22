8b6bba91761aa3cad833f8d48697a722c6ba4db0
totolouis <louismagand@gmail.com>
2016-09-22 15:11:28 +0200

    mis en place du calcul de la norme du vecteur ainsi que de la multiplication v x m

--- src/pca/CalculMatriciel.java
+++ src/pca/CalculMatriciel.java
@@ -5,6 +5,8 @@
  */
 package pca;
 
+import static java.lang.Math.sqrt;
+
 /**
  *
  * @author rayanmehdi1
@@ -15,11 +17,22 @@ public class CalculMatriciel {
     private Vecteur v;
     private double arret;
     
+    //initialise m et v ainsi q'une variable arret
+    
     public void initial(Matrice m, Vecteur v, double arret){
         this.m = m;
         this.v = v;
         this.arret = arret;
     }
+    
+    /* commence l'algo
+    -prendre une matrice
+    -prendre un vecteur (commencé par 1...1)
+    -while |v1 - v2| > arret
+    -multiplié matrice avec vecteur
+    -v = v/norm(v) 
+    -retourné au while
+    */
     
     public void launch(){
         Vecteur v2 = v;
@@ -27,6 +40,7 @@ public class CalculMatriciel {
         while( Math.abs(valeur_propre - valeur_propre_2)> arret){
            v2 = multiplicate();
            v2.aff_vecteur();
+           v2 = norme_vecteur(v2);
         }
     }
     
@@ -41,5 +55,19 @@ public class CalculMatriciel {
             value = 0;
         }
         return test;
+    }
+    
+    public Vecteur norme_vecteur(Vecteur v){
+        double norme = 0;
+        Vecteur vNorme = new Vecteur(v.getTaille());
+        for( int i = 0; i < v.getTaille() ; i++){
+            norme += v.getElement(i) * v.getElement(i);
+        }
+        norme = sqrt(norme);
+        
+        for( int i = 0; i < v.getTaille() ; i++){
+            vNorme.setElement(i, v.getElement(i)/norme);
+        }
+        return vNorme;
     }
 }
