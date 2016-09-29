7ea048ae699a345bdb112e9bb07167b33043b45b
totolouis <louismagand@gmail.com>
2016-09-29 13:42:51 +0100

    mise en place de la boucle while avec la trace

--- src/pca/CalculMatriciel.java
+++ src/pca/CalculMatriciel.java
@@ -5,7 +5,10 @@
  */
 package pca;
 
+
+//Classe eprmettant d'effectuer les calculs
 import static java.lang.Math.sqrt;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,14 +18,16 @@ public class CalculMatriciel {
     //Attributs
     private Matrice m;
     private Vecteur v;
-    private double arret;
+    private double pourcentageTrace;
+    private ArrayList<Double> tabValPropre;
     
     //initialise m et v ainsi q'une variable arret
     
-    public CalculMatriciel(Matrice m, Vecteur v, double arret){
+    public CalculMatriciel(Matrice m, Vecteur v, double pourcentageTrace){
         this.m = m;
         this.v = v;
-        this.arret = arret;
+        this.pourcentageTrace = pourcentageTrace;
+        tabValPropre = new ArrayList();
     }
     
     /* commence l'algo
@@ -34,18 +39,48 @@ public class CalculMatriciel {
     -retourné au while
     */
     
+    /*
+    A faire : prendre la fonction deflation
+    utiliser la nouvelle matrice de deflation dans l'algo
+    pour la valeur de while, calculer la trace et utilisé une aray list composé
+    des valeurs propres. Si 90% de la somme de ces valeurs propres est atteinte 
+    (trace = 100% des valeurs propres)
+    */
+    
     public void calcul_valeurpropre(){//permet de calculer le vecteur propre et la valeur propre de la matrice
         Vecteur v2 = v;
         double valeur_propre = 1, valeur_propre_2 = 0;
-        while( Math.abs(valeur_propre - valeur_propre_2)> arret){
+        double trace = calcul_trace();
+        //while( Math.abs(valeur_propre - valeur_propre_2)> arret){
+        while( sumTab() >= trace*pourcentageTrace){
            valeur_propre = valeur_propre_2;
            v2 = norme_vecteur(v2); //rpz le vecteur B dans lalgo
            v = multiplicate(v2);// rpz le vecteur x dans l'algo
            valeur_propre_2 = transposition(v2,v);
            v2.aff_vecteur();
             System.out.println(valeur_propre_2);
+            tabValPropre.add(valeur_propre_2);
            
         }
+    }
+    
+    public double sumTab(){
+        double sum = 0;
+        for(Double d : tabValPropre){
+            sum += d;
+        }
+        return sum;
+    }
+    
+    public double calcul_trace(){
+        int trace = 0;
+        for( int i = 0; i < m.getColonnes(); i++){
+            for (int j = 0; j < m.getLigne(); j++) {
+                if( i == j )
+                    trace += m.getElement(i, j);
+            }
+        }
+        return trace;
     }
     
     public Vecteur multiplicate(Vecteur v){
